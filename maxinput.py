f6404a9c095eb2b07ab96d7cb08907a54ba8a055
Bhupendra upadhyay <bhupendra.upadhyay_bca24@gla.ac.in>
2025-08-19 15:01:40 +0530

    Add files via upload

---
+++ maxinput.py
@@ -0,0 +1,11 @@
+
+A = float(input("Enter first number (A): "))
+B = float(input("Enter second number (B): "))
+
+
+if( A > B):
+    print("The maximum number is:", A)
+elif (B > A):
+    print("The maximum number is:", B)
+else:
+    print("Both numbers are equal.")
