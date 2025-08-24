246dcf2af37fbfda91eaadf9db6a5957a598db14
Bhupendra upadhyay <bhupendra.upadhyay_bca24@gla.ac.in>
2025-08-24 20:39:10 +0530

    Update triangle1.py

--- triangle1.py
+++ triangle1.py
@@ -4,12 +4,13 @@ b = int(input("Enter second angle: "))
 c = int(input("Enter third angle: "))
 
 
-if a + b + c == 180 and a > 0 and b > 0 and c > 0:
-    if a == 90 or b == 90 or c == 90:
+if (a + b + c == 180 and a > 0 and b > 0 and c > 0):
+    if (a == 90 or b == 90 or c == 90):
         print("Right Triangle")
-    elif a > 90 or b > 90 or c > 90:
+    elif (a > 90 or b > 90 or c > 90):
         print("Obtuse Triangle")
     else:
         print("Acute Triangle")
 else:
     print("Invalid triangle angles")
+
