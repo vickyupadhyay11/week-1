4186610fcc6b715926cec8f49080c9c5237a0554
Bhupendra upadhyay <bhupendra.upadhyay_bca24@gla.ac.in>
2025-08-24 20:39:55 +0530

    Update input.py

--- input.py
+++ input.py
@@ -3,8 +3,9 @@ A=int(input("enter the first number:"))
 B=int(input("enter the second number:"))
 C=int(input("enter the third number:"))
 if(A<B and A<C):
-    print(" Ais the smallest number")
+    print(" A is the smallest number")
 elif(B<C and B<A):
     print("B is the smallest number")
 else:
+
     print("C is the smallest number")
