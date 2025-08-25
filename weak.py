0ea0160ca9a49d22e797a06cafd45394f09c7f5c
Bhupendra upadhyay <bhupendra.upadhyay_bca24@gla.ac.in>
2025-08-25 14:13:20 +0530

    Update weak.py

--- weak.py
+++ weak.py
@@ -1,7 +1,6 @@
-# Read number from user
+
 day_num = int(input("Enter a number (1-7): "))
 
-# Map number to day
 if day_num == 1:
     print("Sunday")
 elif day_num == 2:
@@ -18,3 +17,4 @@ elif day_num == 7:
     print("Saturday")
 else:
     print("Invalid input! Please enter a number between 1 and 7.")
+
